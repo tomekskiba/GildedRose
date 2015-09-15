f64723caada233c9152eac64d26cd82adb343d0c
Tomasz Skiba <tskiba@akamai.com>
2015-09-14 21:01:26 -0400

    commit all code

---
+++ com/gildedrose/GildedRose.java
@@ -0,0 +1,104 @@
+package com.gildedrose;
+
+class GildedRose {
+	Item[] items;
+
+	public final static String CONJURED = "Conjured";
+	
+	public GildedRose(Item[] items) {
+		this.items = items;
+	}
+
+	public void updateQualityOfConjured(Item item ){
+		if(item.sellIn < 0){
+		item.quality = item.quality - 4;
+		}else{
+			item.quality = item.quality - 2;
+		}
+		
+		if(item.quality < 0){
+			item.quality = 0;
+		}
+
+		
+	}
+	
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+
+			if (items[i].name.contains("Conjured")) {
+				updateQualityOfConjured(items[i]);				
+			} else {
+				updateQualityNotConj(items[i]);
+			}
+		}
+	}
+	
+	public void updateQuality(Item item) {
+		if (!item.name.equals("Aged Brie")
+				&& !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+			if (item.quality > 0) {
+				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+					item.quality = item.quality - 1;
+				}
+			}
+		} else {
+			if (item.quality < 50) {
+
+				item.quality = item.quality + 1;
+
+				if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (item.sellIn < 11) {
+						if (item.quality < 50) {
+							item.quality = item.quality + 1;
+						}
+					}
+
+					if (item.sellIn < 6) {
+						if (item.quality < 50) {
+							item.quality = item.quality + 1;
+						}
+					}
+				}
+
+			}
+		}
+
+	}
+	
+	
+	public void updateSellIn(Item item) {
+		if (item.sellIn < 0) {
+			if (!item.name.equals("Aged Brie")) {
+				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					if (item.quality > 0) {
+						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+							item.quality = item.quality - 1;
+						}
+					}
+				} else {
+					item.quality = item.quality - item.quality;
+				}
+			} else {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1;
+				}
+			}
+		}
+	}
+	
+	
+	public void updateQualityNotConj(Item item) {
+		updateQuality(item);
+		
+
+		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			item.sellIn = item.sellIn - 1;
+		}
+		
+		updateSellIn(item);
+
+
+	}
+}
---
+++ com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,61 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void proveValidity() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+        		new Item("Aged Brie", 2, 0),
+        		new Item("Elixir of the Mongoose", 5, 7),
+        		new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47),
+                new Item("Aged BrieSS", -5, 15),
+                new Item("Aged Brie", -5, 15),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+                new Item("Testing", -1, 20),
+                new Item("Conjured test", 10, 18),
+                new Item("Conjured test 1", -2, 1)
+                
+        };
+        
+        
+        
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(80, app.items[0].quality);
+        
+        
+        assertEquals("Aged Brie", app.items[1].name);
+        assertEquals(1, app.items[1].quality);
+        
+        assertEquals("Elixir of the Mongoose", app.items[2].name);
+        assertEquals(6, app.items[2].quality);
+ 
+ 
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[3].name);
+        assertEquals(21, app.items[3].quality);
+ 
+        assertEquals(50, app.items[4].quality);
+        assertEquals(50, app.items[5].quality);
+        assertEquals(50, app.items[8].quality);
+  
+        assertEquals(13, app.items[6].quality);
+        assertTrue(items[4].toString() != null);
+        
+        assertEquals(18,items[9].quality);
+        assertEquals(16,items[10].quality);
+        assertEquals(0,items[11].quality);
+    }
+    
+    
+    
+
+}
